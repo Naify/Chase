cd83720743b00ce81c7dc4043380f3b1e8dc4a5f
Yuriy Naydenov <yuriy.naydenov@pressfire.games>
2024-06-25 15:48:45 +0200

    Initial commit

---
+++ Source/Chase/ChaseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChaseGameMode();
+};
+
+
+
