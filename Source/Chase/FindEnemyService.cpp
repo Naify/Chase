68267945cb1e08449ba525d16c05619467a133c0
Naify <naify@pm.me>
2024-06-27 21:03:15 +0200

    Service 4 finding enemy, chase, player animation fix

---
+++ Source/Chase/FindEnemyService.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FindEnemyService.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "ChaseAIPerceptionComponent.h"
+
+UFindEnemyService::UFindEnemyService()
+{
+}
+
+void UFindEnemyService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	const auto Blackboard = OwnerComp.GetBlackboardComponent();
+
+	if (Blackboard)
+	{
+		const auto Controller = OwnerComp.GetAIOwner();
+		const auto PerceptionComponent = Controller->FindComponentByClass<UChaseAIPerceptionComponent>();
+		if (PerceptionComponent)
+		{
+			Blackboard->SetValueAsObject(EnemyKey.SelectedKeyName, PerceptionComponent->GetClosestEnemy());
+		}
+	}
+	
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
